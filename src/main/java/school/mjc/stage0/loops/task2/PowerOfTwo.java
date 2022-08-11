d1c96b35727179547dd50b1e87cca833ba398c71
rau13 <rauan5005@gmail.com>
2022-08-11 15:35:33 +0600

    ?

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -1,11 +1,14 @@
 package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
-    public void printPower(int power) {
+    public static void main(String[] args){
+        printPower(5);
+    }
+    public static void printPower(int power) {
         int counter = 1;
         while(counter<=power){
             if(counter>0){
-                System.out.println(counter^2);
+                System.out.println((int) Math.pow(2,counter));
             }
             counter++;
         }
