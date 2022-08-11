e90098666724de5a12ab3f368cb0db53a8a7666a
rau13 <rauan5005@gmail.com>
2022-08-11 16:02:10 +0600

    ?

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -1,10 +1,8 @@
 package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
-    public static void main(String[] args){
-        printEvenNumbers(5);
-    }
-    public static void printEvenNumbers(int printTillInclusive) {
+
+    public void printEvenNumbers(int printTillInclusive) {
         int counter = 1;
         while(counter <= printTillInclusive){
             if(counter%2==0){
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,13 +1,27 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
+
     public void printPrimeNumbers(int printToInclusive) {
-        int counter = 1;
-        while(counter <= printToInclusive){
-            if(counter%counter==0 &&  counter%1==0){
-                System.out.println(counter);
+        int primeCheckNumber;
+        int n = 1;
+        int divisibleCount;
+
+        while (n <= printToInclusive) {
+            divisibleCount = 0;
+            primeCheckNumber = 2;
+            while (primeCheckNumber <= n / 2) {
+                if (n % primeCheckNumber == 0) {
+                    divisibleCount++;
+                    break;
+                }
+                primeCheckNumber++;
             }
-            counter++;
+            if (divisibleCount == 0 && n != 1) {
+                System.out.println(n);
+            }
+            n++;
         }
     }
 }
+
